235b506ba76faed24fe3d6038d5643df75c82c61
Sammers21 <titantins@gmail.com>
2024-05-17 13:47:09 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    match clap::Command::new("geralt")
+        .bin_name("geralt")
+        .subcommand_required(true)
+        .subcommand(clap::command!("init"))
+        .subcommand(clap::command!("build"))
+        .subcommand(clap::command!("run"))
+        .get_matches()
+        .subcommand()
+    {
+        Some(("init", _)) => println!("Created binary (application) package"),
+        Some(("build", _)) => println!("Building the fat jar..."),
+        Some(("run", _)) => println!("Running the application..."),
+        _ => println!("No subcommand provided"),
+    };
 }
